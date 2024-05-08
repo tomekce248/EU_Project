f626ab9c536fcbdabd93de7b47b0464c7fd7bdbf
manticore248 <111044446+tomekce248@users.noreply.github.com>
2024-05-08 20:40:09 +0300

    Add files via upload
    EU project V1.0

---
+++ Avrupa_Birliği_Site/js/navbar.js
@@ -0,0 +1,8 @@
+function updateProgressBar(){
+    const {scrollTop, scrollHeight} = document.documentElement;
+    const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
+    document.querySelector('#progress-bar').style.setProperty('--progress', scrollPercent);
+}
+
+
+document.addEventListener('scroll', updateProgressBar);
