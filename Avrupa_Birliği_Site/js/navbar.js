d2268f56bdc0dfba722f5c0d8d475b577afff96d
manticore248 <111044446+tomekce248@users.noreply.github.com>
2024-05-08 20:46:54 +0300

    Delete Avrupa_Birliği_Site directory

--- Avrupa_Birliği_Site/js/navbar.js
+++
@@ -1,8 +0,0 @@
-function updateProgressBar(){
-    const {scrollTop, scrollHeight} = document.documentElement;
-    const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
-    document.querySelector('#progress-bar').style.setProperty('--progress', scrollPercent);
-}
-
-
-document.addEventListener('scroll', updateProgressBar);
